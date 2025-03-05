5b9adeb9d855d108c091b808f179e765623e7193
Prashant Mishra <prashantmishra10232@gmail.com>
2025-03-05 21:46:55 +0530

    added routes for job

---
+++ backend/routes/job.routes.js
@@ -0,0 +1,12 @@
+import express from "express"
+import {getAdminsJobs,getAllJobs,getJobById,postJob} from "../controllers/job.controller.js"
+import verifyJWT from "../middlewares/auth.middleware.js"
+
+const router = express.Router();
+
+router.route("/post").post(verifyJWT, postJob);
+router.route("/get").get(verifyJWT, getAllJobs);
+router.route("/getadminjobs").get(verifyJWT, getAdminsJobs);
+router.route("/get/:id").get(verifyJWT, getJobById);
+
+export default router;
