5e55c88403ce22ccb49caa94fe32bcce57ed9826
Prashant Mishra <prashantmishra10232@gmail.com>
2025-03-05 21:58:32 +0530

    added application routes

---
+++ backend/routes/application.routes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import verifyJWT from "../middlewares/auth.middleware.js"
+import {applyJob,getApplications,getAppliedJobs,updateStatus} from "../controllers/application.controller.js"
+
+const router = express.Router();
+
+router.route("/apply/:id").get(verifyJWT, applyJob);
+router.route("/get").get(verifyJWT, getAppliedJobs);
+router.route("/:id/applicants").get(verifyJWT, getApplications);
+router.route("/status/:id/update").post(verifyJWT, updateStatus);
+ 
+
+export default router;
