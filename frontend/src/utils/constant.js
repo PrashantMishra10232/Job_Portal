66eb884753ef3ea255359574ff83828e8ff09970
PrashantNeolytix10232 <prashant@neolytix.com>
2026-01-02 16:20:25 +0530

    bugs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,13 +1,14 @@
-// export const USER_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/user"
-// export const JOB_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/job"
-// export const APPLICATION_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/application"
-// export const COMPANY_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/company"
+export const USER_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/user"
+export const JOB_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/job"
+export const APPLICATION_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/application"
+export const COMPANY_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/company"
+export const INTERVIEW_API_ENDPOINT="https://chakri-portal.onrender.com/api/v1/admin"
 
 
 
 
-export const USER_API_ENDPOINT="http://localhost:8000/api/v1/user"
-export const JOB_API_ENDPOINT="http://localhost:8000/api/v1/job"
-export const APPLICATION_API_ENDPOINT="http://localhost:8000/api/v1/application"
-export const COMPANY_API_ENDPOINT="http://localhost:8000/api/v1/company"
-export const INTERVIEW_API_ENDPOINT="http://localhost:8000/api/v1/admin"
+// export const USER_API_ENDPOINT="http://localhost:8000/api/v1/user"
+// export const JOB_API_ENDPOINT="http://localhost:8000/api/v1/job"
+// export const APPLICATION_API_ENDPOINT="http://localhost:8000/api/v1/application"
+// export const COMPANY_API_ENDPOINT="http://localhost:8000/api/v1/company"
+// export const INTERVIEW_API_ENDPOINT="http://localhost:8000/api/v1/admin"
