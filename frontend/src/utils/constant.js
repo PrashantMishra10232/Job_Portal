c595676d15da5da0cef1273d562a5f223ebc023e
PrashantNeolytix10232 <prashant@neolytix.com>
2026-02-04 19:16:58 +0530

    bug

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,7 @@
 // For local: http://localhost:8000
 // For production: https://chakri-portal.onrender.com
-// const API_BASE_URL = "https://chakri-portal.onrender.com";
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://chakri-portal.onrender.com";
+// const API_BASE_URL = "http://localhost:8000";
 
 export const USER_API_ENDPOINT = `${API_BASE_URL}/api/v1/user`;
 export const JOB_API_ENDPOINT = `${API_BASE_URL}/api/v1/job`;
